996a77d7c0411c65d85bc7790bcf4ae669032be0
Javi Calonge <javicalongem@gmail.com>
2024-10-19 12:47:55 +0200

    Menu hamburguesa y carpeta Scripts

---
+++ Scripts/menu-hamburguesa.js
@@ -0,0 +1,14 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".nav");
+
+// Asegúrate de que el menú esté oculto al cargar la página
+nav.style.display = "none";
+
+menuToggle.addEventListener("click", () => {
+  // Alterna la visibilidad del menú
+  nav.style.display =
+    nav.style.display === "none" || nav.style.display === "" ? "flex" : "none";
+
+  // Cambia el icono del menú hamburguesa a 'X' cuando esté abierto, y vuelve a '≡' cuando esté cerrado
+  menuToggle.innerHTML = nav.style.display === "flex" ? "✖" : "&#9776;";
+});
